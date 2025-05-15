0ed672d16e22d824e2101420542e1d667e09e9b9
Wongsatorn Sungsilpawech <wongsatorn.sung@hotmail.com>
2025-05-15 14:39:22 +0700

    Initial commit

---
+++ mainDetector.c
@@ -0,0 +1,25 @@
+/*
+ * File:   mainDetector.c
+ * Author: ASUS
+ *
+ * Created on 15 ??????? 2568, 14:34 ?.
+ */
+
+
+#include <xc.h>
+
+void setup(){
+    
+}
+
+void loop(){
+    
+}
+
+void main(void) {
+    setup();
+    while(1){
+        loop();
+    }
+    return;
+}
